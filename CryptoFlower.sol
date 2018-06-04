ba376e2260be97d320a8ba2ec5399b56202ba3dc
josef <josef.jelacic@gmail.com>
2018-06-04 10:18:45 -0400

    ERC20 => ERC721 fully implemented

--- CryptoFlower.sol
+++ CryptoFlower.sol
@@ -1,9 +1,20 @@
 pragma solidity ^0.4.24;
 
-import './contracts/token/ERC721/ERC721BasicToken.sol';
+import './contracts/token/ERC721/ERC721Token.sol';
+import './contracts/ownership/Ownable.sol';
 
-contract CryptoFlower is ERC721BasicToken {
-    function CryptoFlower(){
+contract CryptoFlower is ERC721Token, Ownable {
+    
+    constructor(string _name, string _symbol)
+    ERC721Token(_name, _symbol)
+    public {}
 
+    function lastID() view internal returns (uint256)  {
+        return allTokens.length;
     }
+    
+    function mint(address beneficiary) onlyOwner external returns (bool)  {
+        _mint(beneficiary, lastID());
+        return true; 
+    } 
 }
